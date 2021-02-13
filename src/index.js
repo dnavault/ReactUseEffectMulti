ec05d5622f3f79cc1ff41b2d7038cfc4b2b10886
dnavault <47720676+dnavault@users.noreply.github.com>
2021-02-12 23:06:53 -0400

    more revamp

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "./styles.css";
 function App() {
   const [resp, setData] = useState({ data: null, repos: null });
 
+	
   useEffect(() => {
     const fetchData = async () => {
       const apiEmployeePrefixUrl = 'https://api.hatchways.io/assessment/workers/'
@@ -39,11 +40,26 @@ function App() {
   }, []);
 
  // console.log("render");
-  if (resp.workOrders) {
+ let divWorkOrderList = <div></div>
+ let employee = {};
+ if (resp.workOrders) {
     console.log("d", resp.workOrders, resp.employees);
+
+     divWorkOrderList = resp.workOrders.map((workorder,index) => {
+        console.log("tet",resp.employees.find(x => x.id === 1))
+      return  (<div>
+        <div>{workorder.name}</div>
+        <div>{workorder.description}</div>
+       
+      </div>);
+      });
+
   }
 
-  return <div><h1>Helhssddslos</h1></div>;
+  return (<div>
+    <h1>Work Orde</h1>
+    {divWorkOrderList}
+    </div>);
 }
 
 const rootElement = document.getElementById("root");
